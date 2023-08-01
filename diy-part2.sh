cd1f511f7714249ca94ed1f2507e925b5fd94d18
lpy5511241 <51349704+lpy5511241@users.noreply.github.com>
2023-08-01 23:59:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,20 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1.修改默认ip
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 2.修改主机名
+sed -i 's/OpenWrt/RT4230W/g' package/base-files/files/bin/config_generate
+
+# 4.修改版本号
+sed -i "s/OpenWrt /lpy build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 5.修改默认主题
+# sed -i ' s/luci-theme-bootstrap/luci-theme-argon/g ' feeds/luci/collections/luci/Makefile
+
+# 6.设置ttyd免登录
+# sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+
+# 7.修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
